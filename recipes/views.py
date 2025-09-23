699a4673136c9449f14175cd2852069f99ec3c93
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-23 12:16:47 +0100

    Replace index view with my_recipes view to customize response message

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world!")
+def my_recipes(request):
+    return HttpResponse("Hello, Olciu!")
