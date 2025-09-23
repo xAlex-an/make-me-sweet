c5d9804be2fe998ed670cfc10040ca37d5d3773a
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-23 15:14:57 +0100

    Update ALLOWED_HOSTS to include production domain and localhost

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['make-me-sweet-f649b0b1112e.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
