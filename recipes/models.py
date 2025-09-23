6ba64e4dd5e8df1dc6f18ba73244ea57fe1b7ff3
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-23 15:00:04 +0100

    Remove unused slugify import and clean up Recipe model methods

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.text import slugify
 
 # Create your models here.
 
@@ -23,14 +22,3 @@ class Recipe(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['-created_on']
-
-    def __str__(self):
-        return f"{self.title} | written by {self.author}"
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
