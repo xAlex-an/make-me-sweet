f2714278b56924f614367d0dc46fd9b5764545f5
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-23 12:16:38 +0100

    Update URL patterns to use my_recipes view for the recipes endpoint

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from recipes import views as index_views
+from recipes.views import my_recipes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_views.index, name='index'),
+    path('recipes/', my_recipes, name='recipes'),
 ]
