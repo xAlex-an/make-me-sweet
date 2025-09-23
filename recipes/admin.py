cda24f443ba5b60bb656bcd7b9ae6f9d232651e6
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-23 19:49:27 +0100

    Use SummernoteModelAdmin for Recipe admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Recipe, Comment
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'author__username', 'description', 'ingredients', 'instructions']
+    list_filter = ('status', 'created_on', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description', 'ingredients', 'instructions', 'excerpt')
 
 
 # Register your models here.
-admin.site.register(Recipe)
 admin.site.register(Comment)
