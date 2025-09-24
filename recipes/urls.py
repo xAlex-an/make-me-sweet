cf1c11877890e9f4486c23c2dfdcca836aa148e9
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-24 18:47:49 +0100

    Add URL patterns for editing and deleting comments in recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('<slug:slug>/', views.recipes_detail, name='recipes_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
