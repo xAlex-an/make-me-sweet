ba42037246e328dc1c75cc36bed42ad0daeebc44
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-10-08 12:36:17 +0100

    Fix Python linter errors for clean compliance in admin.py, forms.py, models.py, and urls.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Recipe, Comment
 
+
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ['title', 'author__username', 'description', 'ingredients', 'instructions']
+    search_fields = [
+        'title', 'author__username', 'description',
+        'ingredients', 'instructions'
+    ]
     list_filter = ('status', 'created_on', 'author')
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('description', 'ingredients', 'instructions', 'excerpt')
+    summernote_fields = (
+        'description', 'ingredients', 'instructions', 'excerpt'
+    )
 
 
 # Register your models here.
--- recipes/models.py
+++ recipes/models.py
@@ -50,7 +50,7 @@ class Recipe(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
-    
+
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
@@ -68,7 +68,6 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-
 
     class Meta:
         ordering = ["created_on"]
